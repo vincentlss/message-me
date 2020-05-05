68194ad10e19023ba560197de4336b5385ae5ee7
vincentlss <shyanshinvincent@gmail.com>
2020-05-05 17:57:44 +0800

    Added message controller to broadcast message without websocket

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "user1", password: "password")
+User.create(username: "user2", password: "password")
+User.create(username: "user3", password: "password")
+User.create(username: "user4", password: "password")
